8d1dfba914f393d1a8cc6d3ce557cf2171b03321
harunath <harunath04@gmail.com>
2025-04-30 12:27:22 +0530

    bug mf zones (zone to zones)

--- apps/admin-franchise-app/src/app/(master-franchise)/master-franchise/zones/page.tsx
+++ apps/admin-franchise-app/src/app/(master-franchise)/master-franchise/zones/page.tsx
@@ -6,7 +6,7 @@ const page = () => {
 	return (
 		<div className="flex flex-col items-center p-8 space-y-4 bg-gray-100">
 			<Link
-				href="/master-franchise/zone/create"
+				href="/master-franchise/zones/create"
 				className="px-4 py-2 bg-red-600 text-white  shadow-md hover:bg-red-700 transition duration-300 ease-in-out w-max">
 				Create Zone
 			</Link>
